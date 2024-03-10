38ac537b0051482b32375bedc3b279317a57e398
rehans-life <rehantosif4@gmail.com>
2024-03-10 14:29:20 +0300

    added man.yaml file

--- server/src/utils/axios.ts
+++ server/src/utils/axios.ts
@@ -3,10 +3,10 @@ import keys from "../../config/keys";
 
 const instance = axios.create({
     baseURL: keys.JUDGE0_API,
-    headers: {
+    headers: process.env.environment === "Development" ? {
         'X-RapidAPI-Key': keys.RAPID_API_KEY,
         'X-RapidAPI-Host': keys.RAPID_API_HOST
-    }
+    } : {}
 })
 
 export default instance
