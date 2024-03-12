a8acc31ceeeea09d1ba2d7e449d945c8ee427bfb
rehans-life <rehantosif4@gmail.com>
2024-03-12 17:20:36 +0300

    added environment variables to client dockerfile

--- client/firebase.ts
+++ client/firebase.ts
@@ -3,8 +3,7 @@ import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_URI,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
 }
 
 const app = initializeApp(firebaseConfig)
