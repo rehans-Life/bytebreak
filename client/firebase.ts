28b8caa63e03fc99fdd16f0478c706f981b4bab6
rehans-life <rehantosif4@gmail.com>
2024-03-14 09:49:21 +0300

    Added NEXT_PUBLIC prefix to client env vars

--- client/firebase.ts
+++ client/firebase.ts
@@ -2,8 +2,8 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
 }
 
 const app = initializeApp(firebaseConfig)
