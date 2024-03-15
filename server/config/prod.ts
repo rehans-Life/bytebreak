459cc3711b8a9fd27dbac59c9ff257345964030b
rehans-life <rehantosif4@gmail.com>
2024-03-15 18:48:49 +0300

    removed _FILE suffix from prod keys

--- server/config/prod.ts
+++ server/config/prod.ts
@@ -5,13 +5,12 @@ import { join } from 'path'
 const secret_path = '/run/secrets/'
 
 const getValue = (name: string) =>
-  readFileSync(join(secret_path, `${name}_FILE`), 'utf-8')
+  readFileSync(join(secret_path, `${name}`), 'utf-8')
 
 export default {
   PORT: process.env.PORT || '4000',
   NODE_ENV: 'Production',
-  DB_URI:
-    getValue('DB_URI')?.replace('<password>', getValue('DB_PASSWORD')) || '',
+  DB_URI: getValue('DB_URI').replace('<password>', getValue('DB_PASSWORD')),
   JWT_SECRET_KEY: getValue('JWT_SECRET_KEY'),
   JWT_EXPIRES_IN: getValue('JWT_EXPIRES_IN'),
   JWT_COOKIE_EXPIRES_IN: getValue('JWT_COOKIE_EXPIRES_IN'),
